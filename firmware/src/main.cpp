a278c1d20a76714b7c704d866648f6e47ad90e87
bigalex <bigalex@gmx.de>
2020-03-08 18:28:38 +0100

    remove stepper lib

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,7 +6,6 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <ESP32Servo.h> 
-#include <A4988.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -25,13 +24,6 @@ U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SD
 #define ENCODER_PIN_B GPIO_NUM_32
 Rotary r = Rotary(ENCODER_PIN_A, ENCODER_PIN_B);  //rotary encoder on pins GPIO32 and GPIO33
 
-#define SUN	0
-#define SUN_CLOUD  1
-#define CLOUD 2
-#define RAIN 3
-#define THUNDER 4
-
-
 
 int position = 0;
 
@@ -45,14 +37,12 @@ int position = 0;
 // Acceleration and deceleration values are always in FULL steps / s^2
 #define MOTOR_ACCEL 200
 #define MOTOR_DECEL 100
-#define DIR GPIO_NUM_16
-#define STEP GPIO_NUM_17
-#define SLEEP GPIO_NUM_18
-#define MS1 GPIO_NUM_25
-#define MS2 GPIO_NUM_26
-#define MS3 GPIO_NUM_27
-
-A4988 stepper(MOTOR_STEPS, DIR, STEP, SLEEP, MS1, MS2, MS3);
+#define STEPPER_DIR GPIO_NUM_16
+#define STEPPER_STEP GPIO_NUM_17
+#define STEPPER_SLEEP GPIO_NUM_18
+#define STEPPER_MS1 GPIO_NUM_25
+#define STEPPER_MS2 GPIO_NUM_26
+#define STEPPER_MS3 GPIO_NUM_27
 
 // ### SERVO ###
 // Hobbyking HK15148 Analog Servo 2.5kg / 0.14sec / 17
@@ -73,41 +63,9 @@ const char* password = WIFI_PASSWD;
 
 bool OTA_in_Progress = false;
 
-void drawWeatherSymbol(u8g2_uint_t x, u8g2_uint_t y, uint8_t symbol)
-{
-  // fonts used:
-  // u8g2_font_open_iconic_embedded_6x_t
-  // u8g2_font_open_iconic_weather_6x_t
-  // encoding values, see: https://github.com/olikraus/u8g2/wiki/fntgrpiconic
-  
-  switch(symbol)
-  {
-    case SUN:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 69);	
-      break;
-    case SUN_CLOUD:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 65);	
-      break;
-    case CLOUD:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 64);	
-      break;
-    case RAIN:
-      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
-      u8g2.drawGlyph(x, y, 67);	
-      break;
-    case THUNDER:
-      u8g2.setFont(u8g2_font_open_iconic_embedded_2x_t);
-      u8g2.drawGlyph(x, y, 79);
-      break;      
-  }
-}
 
-void drawWeather(uint8_t symbol, int degree)
+void drawWeather(int degree)
 {
-  drawWeatherSymbol(0, 42, symbol);
   u8g2.setFont(u8g2_font_logisoso16_tf);
   u8g2.setCursor(16+3, 42);
   u8g2.print(degree);
@@ -115,11 +73,11 @@ void drawWeather(uint8_t symbol, int degree)
 }
 
 
-void draw(uint8_t symbol, int degree)
+void draw(int degree)
 {
   u8g2.firstPage();
   do {
-    drawWeather(symbol, degree);
+    drawWeather(degree);
   } while ( u8g2.nextPage() );
 }
 
@@ -131,21 +89,27 @@ void IRAM_ATTR rotaryChanged() {
     }
 }
 
-void setup(void) {
-  Serial.begin(115200);
-  myservo.setPeriodHertz(50);// Standard 50hz servo
-  myservo.attach(SERVO_PIN, 500, 2400); 
-  
-  u8g2.begin();   
-  u8g2.enableUTF8Print();
-  r.begin(true);
-  attachInterrupt(digitalPinToInterrupt(GPIO_NUM_32), rotaryChanged, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(GPIO_NUM_33), rotaryChanged, CHANGE);
-  stepper.begin(RPM);
-  stepper.setSpeedProfile(stepper.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
-  stepper.enable();
 
+
+
+void setup(void) {
+// configure stepper driver A4988
+  pinMode(STEPPER_SLEEP, OUTPUT);
+  digitalWrite(STEPPER_SLEEP, LOW);
+  pinMode(STEPPER_DIR, OUTPUT);
+  digitalWrite(STEPPER_DIR, HIGH);
+  pinMode(STEPPER_STEP, OUTPUT);
+  digitalWrite(STEPPER_STEP, LOW);
+  // 400 Half-Steps
+  pinMode(STEPPER_MS1, OUTPUT);
+  digitalWrite(STEPPER_MS1, HIGH);
+  pinMode(STEPPER_MS2, OUTPUT);
+  digitalWrite(STEPPER_MS2, LOW);
+  pinMode(STEPPER_MS3, OUTPUT);
+  digitalWrite(STEPPER_MS3, LOW);
+  Serial.begin(115200);
   Serial.println("Booting");
+ 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -166,99 +130,62 @@ void setup(void) {
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-ArduinoOTA
-    .onStart([]() {
-      OTA_in_Progress = true;
-      myservo.detach();
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+  ArduinoOTA
+      .onStart([]() {
+        OTA_in_Progress = true;
+        myservo.detach();
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() {
+        Serial.println("\nEnd");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+      });
   ArduinoOTA.begin();
 
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  myservo.setPeriodHertz(50);// Standard 50hz servo
+  myservo.attach(SERVO_PIN, 500, 2400); 
+  
+  u8g2.begin();   
+  u8g2.enableUTF8Print();
+  r.begin(true);
+  attachInterrupt(digitalPinToInterrupt(GPIO_NUM_32), rotaryChanged, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(GPIO_NUM_33), rotaryChanged, CHANGE);
+  
+
 }
 
 void loop(void) {
-
-  draw(THUNDER, position);
-  //draw("The sun's come out!", SUN_CLOUD, 19);
-  //draw("It's raining cats and dogs.", RAIN, 8);
-  //draw("That sounds like thunder.", THUNDER, 12);
-  //draw("It's stopped raining", CLOUD, 15);
-
-    delay(1000);
-
-    /*
-     * Moving motor in full step mode is simple:
-     */
-    stepper.setMicrostep(1);  // Set microstep mode to 1:1
-
-    // One complete revolution is 360°
-    stepper.rotate(360);     // forward revolution
-    delay(1000);
-    //stepper.rotate(-360);    // reverse revolution
-    //delay(1000);
-    // One complete revolution is also MOTOR_STEPS steps in full step mode
-    //stepper.move(MOTOR_STEPS*3);    // forward revolution
-    //delay(1000);
-    //stepper.move(-MOTOR_STEPS);   // reverse revolution
-    //delay(1000);
-    /*
-     * Microstepping mode: 1, 2, 4, 8, 16 or 32 (where supported by driver)
-     * Mode 1 is full speed.
-     * Mode 32 is 32 microsteps per step.
-     * The motor should rotate just as fast (at the set RPM),
-     * but movement precision is increased, which may become visually apparent at lower RPMs.
-     */
-    stepper.setMicrostep(8);   // Set microstep mode to 1:8
-
-    // In 1:8 microstepping mode, one revolution takes 8 times as many microsteps
-    
-    stepper.move(8 * MOTOR_STEPS*3);    // forward revolution
-    delay(1000);
-    //stepper.move(-8 * MOTOR_STEPS);   // reverse revolution
-    //delay(1000);
-    // One complete revolution is still 360° regardless of microstepping mode
-    // rotate() is easier to use than move() when no need to land on precise microstep position
-    //stepper.rotate(360);
-    //delay(1000);
-    //stepper.rotate(-360);
-
-
+  digitalWrite(STEPPER_SLEEP, HIGH);
+  delay(1000);
   for(;;){
-
-      draw(CLOUD, position);
-      ArduinoOTA.handle();
-      if(position<=180 && position>=0) {
-        if (!OTA_in_Progress){
-          myservo.write(position);
-          
-        }
-      }
-      delay(20);
+    draw(position);
+    //myservo.write(position);
+    ArduinoOTA.handle();
+    delay(200);
+    digitalWrite(STEPPER_STEP, HIGH);
+    delay(200);
+    digitalWrite(STEPPER_STEP, LOW);   
     
   }
     //Serial.println("foo"); 
@@ -272,3 +199,11 @@ void loop(void) {
 
 
 // https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
+
+
+// https://github.com/laurb9/StepperDriver = StepperDriver@1.2.0
+// is blocking while waiting for the next pulse and can not handle
+// speed changes during move
+
+// Maybe https://github.com/Stan-Reifel/FlexySteppe = FlexyStepper@1.0.0
+// is a better solution?
